0986e6027b2d95d9f9b1442b77dfac024f45f4d5
theodorejohn7 <theodorejohn.s@10decoders.in>
2023-01-18 22:52:20 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import "./global.css";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
