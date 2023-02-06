9c046f4e9598bb01ff66c3c579d23dc9ad0bddce
theodorejohn7 <theodorejohn.s@10decoders.in>
2023-02-07 01:59:28 +0530

    added responsive navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,55 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
+
+
+export const menuItems = [
+  { name: "Overview", path: "./images/navbar/apps__overview.svg", altText: "Overview Icon" },
+  { name: "Produts", path: "./images/navbar/grip-horizontal__products.svg", altText: "Products Icon" },
+  { name: "Orders", path: "./images/navbar/tag__orders.svg", altText: "Orders Icon" },
+  { name: "Reports", path: "./images/navbar/brand-google-analytics__reports.svg", altText: "Reports Icon" },
+  { name: "Setting", path: "./images/navbar/settings.svg", altText: "Reports Icon" },
+  { name: "Adminstration", path: "./images/navbar/user__administration.svg", altText: "Adminstration Icon" },
+  { name: "Maintenance", path: "./images/navbar/tool__Maintenance.svg", altText: "Maintenance Icon" },
+  { name: "Help", path: "./images/navbar/aperture__Help.svg", altText: "Help Icon" },
+]
 
 const Navbar = () => {
   const [open, setOpen] = useState(true)
 
-  let menuItems = [
-    { name: "Overview", path: "./images/navbar/apps__overview.svg", altText: "Overview Icon" },
-    { name: "Produts", path: "./images/navbar/grip-horizontal__products.svg", altText: "Products Icon" },
-    { name: "Orders", path: "./images/navbar/tag__orders.svg", altText: "Orders Icon" },
-    { name: "Reports", path: "./images/navbar/brand-google-analytics__reports.svg", altText: "Reports Icon" },
-    { name: "Setting", path: "./images/navbar/settings.svg", altText: "Reports Icon" },
-    { name: "Adminstration", path: "./images/navbar/user__administration.svg", altText: "Adminstration Icon" },
-    { name: "Maintenance", path: "./images/navbar/tool__Maintenance.svg", altText: "Maintenance Icon" },
-    { name: "Help", path: "./images/navbar/aperture__Help.svg", altText: "Help Icon" },
-  ]
 
   return (
-    <div className={`hidden md:block bg-blackRock min-h-screen ${open ? `min-w-max` : `w-88`} duration-500`}>
-      <div className="pt-6 pb-102 flex  mx-4 ">
-        
+    <>
+       
+
+
+      <div className={`hidden md:block bg-blackRock min-h-screen ${open ? `min-w-max` : `w-88`} duration-500`}>
+        <div className="pt-6 pb-102 flex  mx-4 ">
+
           <img src="./images/navbar/align-justified_menu.svg" alt="Menu button" className="h-6" onClick={() => setOpen(!open)} />
-         
-        <img src="./images/navbar/Logo.svg" alt="Seller Center Logo" className={`pl-5  w-44 h-8 ${!open && ` hidden translate-x-28`}`} />
-      </div>
-      {
-        menuItems.map((menu) =>
-          <div className={"flex group  hover:bg-midBlack"}>
-            <div className="pl-0.5 bg-white  invisible group-hover:visible ">
-            </div>
-            <div className="flex w-full justify-between mb-1  bg-mildGrey mx-3.5 py-4 ">
-              <div className=" flex">
-                <img src={`${menu.path}`} alt={`${menu.altText}`} className="pl-5 pr-2  " />
-                <p className={`text-white pl-2  ${!open && `opacity-0`}`} >{menu.name}</p>
+
+          <img src="./images/navbar/Logo.svg" alt="Seller Center Logo" className={`pl-5  w-44 h-8 ${!open && ` hidden translate-x-28`}`} />
+        </div>
+        {
+          menuItems.map((menu) =>
+            <div className={"flex group  hover:bg-midBlack"}>
+              <div className="pl-0.5 bg-white  invisible group-hover:visible ">
               </div>
-              <img className="pr-4    " src="./images/navbar/chevron-right__arrow.svg" alt="arrowIcon" />
+              <div className="flex w-full justify-between mb-1  bg-mildGrey mx-3.5 py-4 ">
+                <div className=" flex">
+                  <img src={`${menu.path}`} alt={`${menu.altText}`} className="pl-5 pr-2  " />
+                  <p className={`text-white pl-2  ${!open && `opacity-0`}`} >{menu.name}</p>
+                </div>
+                <img className="pr-4    " src="./images/navbar/chevron-right__arrow.svg" alt="arrowIcon" />
+              </div>
             </div>
-          </div>
-        )
-      }
-    </div>
+          )
+        }
+      </div>
+
+
+
+
+    </>
+
   );
 };
 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { menuItems } from '../Navbar/Navbar';
 
 const SearchBar = () => {
+    const [open, setOpen] = useState(false)
+
     return (
-        <div>
+        < >
 
             <div className='hidden md:block w-1186 '>
 
@@ -24,7 +27,51 @@ const SearchBar = () => {
                     </div>
                 </div>
             </div>
-        </div>
+            <div className=' flex justify-between md:hidden h-62_ bg-blackRock mb-6'>
+                <div>
+
+                    <div className={`block md:hidden bg-blackRock  md:min-h-screen ${open ? `min-w-max absolute z-50` : `w-88 h-6`} duration-500`}>
+                        <div className="pt-6 pb-6 flex  mx-4 ">
+
+                            <img src="./images/navbar/align-justified_menu.svg" alt="Menu button" className="h-6" onClick={() => setOpen(!open)} />
+
+                            <img src="./images/navbar/Logo.svg" alt="Seller Center Logo" className={`pl-5  w-44 h-8 ${!open && ` hidden translate-x-28`}`} />
+                        </div>
+                        {
+                            menuItems.map((menu) =>
+                                <div className={` ${open ? `flex` : `hidden`} group  hover:bg-midBlack`}>
+                                    <div className="pl-0.5 bg-white  invisible group-hover:visible ">
+                                    </div>
+                                    <div className="flex w-full justify-between mb-1  bg-mildGrey mx-3.5 py-4 ">
+                                        <div className=" flex">
+                                            <img src={`${menu.path}`} alt={`${menu.altText}`} className="pl-5 pr-2  " />
+                                            <p className={`text-white pl-2  ${!open && `opacity-0`}`} >{menu.name}</p>
+                                        </div>
+                                        <img className="pr-4    " src="./images/navbar/chevron-right__arrow.svg" alt="arrowIcon" />
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+
+                <div className='flex  '>
+                    <img src="./images/searchbar/searchWhite.svg" alt="Search button" className="  my-auto mr-66_" />
+                    <img src="./images/searchbar/bellWhite.svg" alt="bell button" className=" mr-4 my-auto  " />
+                    <div className='mt-4 mr-4'>
+                        <p className='text-xs text-white font-semibold leading-3'>
+                            Puma Official Store
+                        </p>
+                        <p className='text-10_ text-mildSandal font-semibold text-right'>
+                            Change Seller
+                        </p>
+                    </div>
+                    <div className='mt-13_  pr-6 '>
+                        <img src="./images/searchbar/status.svg" alt="status pic" className=" w-36_ h-36_  m-0 p-0  " />
+                    </div>
+                </div>
+            </div>
+        </>
 
     )
 }
